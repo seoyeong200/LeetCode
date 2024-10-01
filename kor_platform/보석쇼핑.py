a44b301a639c7c4ac2bbcde96f2d3f47d6ad8362
seoyeong200 <youngseogimkk@gmail.com>
2024-10-01 19:18:03 +0900

    보석쇼핑 - 정확도 100, 효율 0

--- kor_platform/보석쇼핑.py
+++ kor_platform/보석쇼핑.py
@@ -1,16 +1,17 @@
 def solution(gems):
+    answer = []
     uniq_cnt = len(set(gems))
     
     l, r = 0, 1
-
     while r <= len(gems):
         # r 움직여서 gem 추가, 갖고있는 gem 종류 다 만족하는지 확인
         if len(set(gems[l:r])) == uniq_cnt:
             # l 최대한 뒤로
             l += 1
             if len(set(gems[l:r])) != uniq_cnt:
-                return [l, r]
+                answer.append((r-l+1, [l, r]))
+                r += 1
         else:
             r += 1
-        
-    return [None]
+            
+    return answer.sort(key=lambda x: (x[0], x[1][0]))[0][1]
