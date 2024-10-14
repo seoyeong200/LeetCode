7eb68e0fdd2a2b7ae2b2bc2e2a57103f99c863ee
seoyeong200 <youngseogimkk@gmail.com>
2024-10-14 20:35:16 +0900

    max_counter

--- codility/6.max_counters.py
+++ codility/6.max_counters.py
@@ -28,3 +28,29 @@ def solution_time_exceed(N, A):
             counter[A[i]-1] += 1
             max_cnt = max(max_cnt, counter[A[i]-1])
     return counter
+
+def solution(N, A):
+    from collections import Counter
+
+    max_log = []
+    increase_ops = ''.join(list(map(str, A))).split(str(N+1))
+
+    for x in increase_ops:
+        if x == '': continue # max operation 붙어있거나 그걸로 끝나는 경우
+        dix = dict(Counter(x))
+        max_log.append(max(dix.values()))
+
+    # max op 으로 끝난 경우 처리
+    last_op_result = max_log.pop()
+    if A[-1] == N+1: return [last_op_result[-1]] * N
+
+    # max operation이 존재했었음
+    if max_log: 
+        ans = [max_log[-1]] * N
+    # max operation이 존재하지 않았었음
+    else:
+        ans = [0] * N
+    for key, val in dix.items():
+        ans[int(key)-1] += val
+
+    return ans
